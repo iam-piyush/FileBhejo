7c7aaded9f648cf5ddcb4132ef948d11d0415240
iam-piyush <pkpiyush988@gmail.com>
2024-02-02 06:34:36 +0530

    good to publish 01

--- src/Components/ProgressBar.js
+++ src/Components/ProgressBar.js
@@ -1,48 +1,87 @@
 import React from "react";
+import { CheckCircle } from "lucide-react";
 
 export default function ProgressBar({ progress = 10 }) {
-  const strokeWidth = 8;
+  const strokeWidth = 10;
   const radius = 40;
   const circumference = 2 * Math.PI * radius;
   const offset = circumference - (progress / 100) * circumference;
 
   return (
-    <svg
-      className="mt-3"
-      height={radius * 2}
-      width={radius * 2}
-      viewBox={`0 0 ${radius * 2} ${radius * 2}`}
-    >
-      <circle
-        className="progress-ring__circle"
-        strokeWidth={strokeWidth}
-        fill="transparent"
-        r={radius - strokeWidth / 2}
-        cx={radius}
-        cy={radius}
-      />
-      <circle
-        className="progress-ring__progress"
-        strokeWidth={strokeWidth}
-        fill="transparent"
-        r={radius - strokeWidth / 2}
-        cx={radius}
-        cy={radius}
-        style={{
-          strokeDasharray: `${circumference} ${circumference}`,
-          strokeDashoffset: offset,
-        }}
-      />
-      <text
-        x="50%"
-        y="50%"
-        dominantBaseline="middle"
-        textAnchor="middle"
-        fontSize="12px"
-        fill="#000"
-      >
-        {`${progress}%`}
-      </text>
-    </svg>
+    <div style={{ position: "relative" }}>
+      {progress < 100 && (
+        <svg
+          height={radius * 0.8}
+          width={radius * 0.8}
+          viewBox={`0 0 ${radius * 2} ${radius * 2}`}
+        >
+          {/* Background Circle */}
+          <circle
+            className="progress-ring__circle"
+            strokeWidth={strokeWidth}
+            fill="transparent"
+            stroke="#e0e0e0" // Set background color here
+            r={radius - strokeWidth / 2}
+            cx={radius}
+            cy={radius}
+          />
+
+          {/* Progress Circle */}
+          <circle
+            className="progress-ring__progress"
+            strokeWidth={strokeWidth}
+            fill="transparent"
+            stroke="#3C82F6" // Set progress color here
+            r={radius - strokeWidth / 2}
+            cx={radius}
+            cy={radius}
+            style={{
+              strokeDasharray: `${circumference} ${circumference}`,
+              strokeDashoffset: offset,
+            }}
+          />
+        </svg>
+      )}
+
+      {progress === 100 && (
+        <div
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-100%, 0%)",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100%", // Ensure the containing div takes the full height
+          }}
+        >
+          <CheckCircle size={30} color="#3C82F6" />
+        </div>
+      )}
+
+      {progress < 100 && (
+        // Text
+        <div
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            width: "100%",
+            height: "100%",
+          }}
+        >
+          <text style={{fontSize: '10px'}}
+            
+          >
+            {`${Number(progress).toFixed(0)}%`}
+          </text>
+        </div>
+      )}
+    </div>
   );
 }
--- src/Components/UploadFiles.js
+++ src/Components/UploadFiles.js
@@ -1,20 +1,55 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { UploadCloud } from "lucide-react";
 import Alert from "./Alert";
 import FilePreview from "./FilePreview";
 import { app } from "../firebase";
 import {
+    deleteObject,
   getDownloadURL,
   getStorage,
   ref,
   uploadBytesResumable,
 } from "firebase/storage";
-import ProgressBar from "./ProgressBar";
+import QRCode from "react-qr-code";
 
 export default function UploadFiles() {
   const [files, setFiles] = useState([]);
   const [errorMsg, setErrorMsg] = useState();
-  const [progress, setProgress] = useState();
+  const [progressArray, setProgressArray] = useState([]);
+  const [uploaded, setUploaded] = useState(false);
+  const [downloadURL, setDownloadURL] = useState("");
+
+  const [timeRemaining, setTimeRemaining] = useState({ minutes: 3, seconds: 0 });
+
+  useEffect(() => {
+    let timer;
+
+    if (uploaded) {
+      // Set a timer to update the countdown and delete the file after 3 minutes
+      timer = setInterval(() => {
+        setTimeRemaining((prevTime) => {
+          if (prevTime.seconds > 0) {
+            return { ...prevTime, seconds: prevTime.seconds - 1 };
+          } else if (prevTime.minutes > 0) {
+            return { minutes: prevTime.minutes - 1, seconds: 59 };
+          } else {
+            deleteFileFromStorage();
+            return prevTime;
+          }
+        });
+      }, 1000);
+
+      setTimeout(() => {
+        deleteFileFromStorage();
+        setUploaded(false); // Set uploaded to false to trigger the cleanup in useEffect
+      }, 180000); // 3 minutes (180,000 milliseconds)
+    }
+
+    return () => {
+      // Clear the timers when the component unmounts or when a new file is uploaded
+      clearInterval(timer);
+    };
+  }, [uploaded]);
 
   const onFileSelect = (selectedFiles) => {
     if (selectedFiles.length === 0) {
@@ -40,6 +75,7 @@ export default function UploadFiles() {
       }
 
       newFiles.push(file);
+      setProgressArray((prev) => [...prev, 0]); // Initialize progress to 0 for each file
     }
 
     setErrorMsg(null);
@@ -50,77 +86,142 @@ export default function UploadFiles() {
 
   const handleUpload = () => {
     const storage = getStorage(app);
-
-    files.forEach((file) => {
+  
+    files.forEach((file, index) => {
       const metadata = {
         contentType: file.type,
       };
       const storageRef = ref(storage, `files/${file.name}`);
       const uploadTask = uploadBytesResumable(storageRef, file, metadata);
-
+  
+      uploadTask
+        .then((snapshot) => {
+          // Upload is complete, retrieve download URL
+          return getDownloadURL(snapshot.ref);
+        })
+        .then((url) => {
+          console.log(`File ${file.name} available at ${url}`);
+          setDownloadURL(url);
+          setUploaded(true);
+        })
+        .catch((error) => {
+          console.error("Error during file upload:", error);
+          setErrorMsg("Error during file upload");
+        });
+  
       uploadTask.on("state_changed", (snapshot) => {
-        const progress =
-          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
         console.log(`Upload for ${file.name} is ${progress}% done`);
-        setProgress(progress);
-        progress == 100 &&
-          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-            console.log("File available at", downloadURL);
-          });
+        setProgressArray((prev) => {
+          const newArray = [...prev];
+          newArray[index] = progress;
+          return newArray;
+        });
       });
     });
   };
 
+  useEffect(() => {
+    let timer;
+
+    if (uploaded) {
+      timer = setTimeout(() => {
+        deleteFileFromStorage();
+      }, 180000);
+    }
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [uploaded]);
+
+  const deleteFileFromStorage = () => {
+    const storage = getStorage(app);
+    const storageRef = ref(storage, `files/${files[0].name}`);
+
+    deleteObject(storageRef)
+      .then(() => {
+        console.log("File deleted successfully");
+        setDownloadURL("");
+        setUploaded(false);
+        setFiles([]);
+        setProgressArray([]);
+      })
+      .catch((error) => {
+        console.error("Error deleting file:", error);
+      });
+  };
+  
+
   return (
     <div>
-      <div className="col-span-full">
-        <div className="mt-2 flex justify-center bg-transparent rounded-lg border border-dashed border-gray-900/25 px-6 py-24">
-          <div className="text-center">
-            <UploadCloud
-              className="mx-auto h-12 w-12 text-gray-300"
-              aria-hidden="true"
-            />
-            <div className="mt-4 flex text-sm leading-6 text-gray-600">
-              <label
-                htmlFor="file-upload"
-                className="relative cursor-pointer rounded-md  font-semibold text-blue-600 hover:text-blue-500"
-              >
-                <span>Upload files</span>
-                <input
-                  id="file-upload"
-                  name="file-upload"
-                  type="file"
-                  className="sr-only"
-                  onChange={(event) => onFileSelect(event.target.files)}
-                  multiple
-                />
-              </label>
-              <p className="pl-1">or drag and drop</p>
+      {!uploaded ? (
+        <div className="drop col-span-full">
+          <div className="mt-2 flex justify-center bg-transparent rounded-lg border border-dashed border-gray-900/25 px-6 py-24">
+            <div className="text-center">
+              <UploadCloud
+                className="mx-auto h-12 w-12 text-gray-300"
+                aria-hidden="true"
+              />
+              <div className="mt-4 flex text-sm leading-6 text-gray-600">
+                <label
+                  htmlFor="file-upload"
+                  className="relative cursor-pointer rounded-md  font-semibold text-blue-600 hover:text-blue-500"
+                >
+                  <span>Upload files</span>
+                  <input
+                    id="file-upload"
+                    name="file-upload"
+                    type="file"
+                    className="sr-only"
+                    onChange={(event) => onFileSelect(event.target.files)}
+                    multiple
+                  />
+                </label>
+                <p className="pl-1">or drag and drop</p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      ) : (
+        <div className="drop col-span-full">
+          <div className="mt-2 flex justify-center bg-transparent rounded-lg border border-dashed border-gray-900/25 py-12">
+            <div className="text-center">
+              <QRCode value={downloadURL} size={128} className="mx-auto" />
+              <p className="mt-4 text-blue-600 font-semibold">
+                <a href={downloadURL} target="_blank" rel="noopener noreferrer">
+                  Download URL
+                </a>
+              </p>
+              {/* Render the countdown timer */}
+              <p className="mt-2 text-gray-500">
+                QR Code valid for:- {timeRemaining.minutes}:
+                {timeRemaining.seconds < 10
+                  ? `0${timeRemaining.seconds}`
+                  : timeRemaining.seconds}{" "}
+              </p>
+            </div>
+          </div>
+        </div>
+      )}
 
       {files.map((file, index) => (
-        <FilePreview key={index} file={file} />
+        <FilePreview key={index} file={file} progress={progressArray[index]} />
       ))}
 
       {errorMsg ? <Alert msg={errorMsg} /> : null}
 
       <div className="mt-4"></div>
-      {progress > 0 ? (
-        <ProgressBar progress={progress} />
-      ) : (
-        <button
-          onClick={handleUpload}
-          disabled={isUploadButtonDisabled}
-          className={`bg-blue-500 text-white px-4 py-2 rounded ${
-            isUploadButtonDisabled ? "cursor-not-allowed opacity-50" : ""
-          }`}
-        >
-          Upload
-        </button>
-      )}
+
+      <button
+        onClick={handleUpload}
+        disabled={isUploadButtonDisabled}
+        className={`bg-blue-500 text-white px-4 py-2 rounded ${
+          isUploadButtonDisabled ? "cursor-not-allowed opacity-50" : ""
+        }`}
+      >
+        Upload
+      </button>
     </div>
   );
 }
