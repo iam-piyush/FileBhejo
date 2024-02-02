fe22a87685d383a2c2365891a19dc302deef602f
iam-piyush <pkpiyush988@gmail.com>
2024-02-03 02:30:54 +0530

    some changes

--- src/Components/DonotWorry.js
+++ src/Components/DonotWorry.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const DonotWorry = () => {
   return (
-    <div className='flex text-center justify-center bg-green-200 py-2 px-1 border border-dashed border-green-600 rounded'>
+    <div className='flex text-center justify-center items-center bg-green-200 py-2 px-1 border border-dashed border-green-600 rounded'>
       <p className='font-bold'>Don't Worry!</p>
       <p className='ml-2'>Your files will be automatically deleted from the server after 5 minutes.</p>
     </div>
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export function Footer() {
   return (
-    <footer className="mt-4 bg-orange-50 py-5 px-1 text-center border rounded border-dashed border-orange-500">
+    <footer className=" bg-orange-50 py-5 px-1 text-center border-t-0 border-b-0 border rounded border-dashed border-orange-500">
       <p>© 2024 FileBhejo. All rights reserved.</p>
       <p className="mt-2">
         Designed by <a href="https://iamritika.vercel.app/" target="blank"><font className="font-medium">Ritika Bhatia</font></a> |
