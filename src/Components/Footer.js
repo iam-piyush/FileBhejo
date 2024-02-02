37d423361b2a5ae5b70b066073f1ce229bbf2d1a
iam-piyush <pkpiyush988@gmail.com>
2024-02-03 02:41:55 +0530

    width changed

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export function Footer() {
   return (
-    <footer className=" bg-orange-50 py-5 mx-auto px-1 text-center border-t-0 border-b-0 border rounded border-dashed border-orange-500 max-[640px]:w-11/12">
+    <footer className=" bg-orange-50 w-full py-5 mx-auto px-1 text-center border-t-0 border-b-0 border rounded border-dashed border-orange-500 max-[640px]:w-11/12">
       <p>© 2024 FileBhejo. All rights reserved.</p>
       <p className="mt-2">
         Designed by <a href="https://iamritika.vercel.app/" target="blank"><font className="font-medium">Ritika Bhatia</font></a> |
